8fa5069a9c115a29d36bf340ede1da1d80ea7b4c
Pedro Abreu <abreu223@hotmail.com>
2022-06-21 11:26:16 -0400

    Comments functorializer.v

--- examples/functorializer.v
+++ examples/functorializer.v
@@ -1,20 +1,22 @@
 From idt Require Import all.
 From MetaCoq.Template Require Import utils All.
 
-Notation "'unfolded' d" :=
-  ltac:(let y := eval unfold d in d in exact y) (at level 100, only parsing).
+(* I'd suggest to re-define any datatype being functorialized *)
+(* This way we can avoid any issues with universes and whatnot *)
+(* Also notice that so far we work solely with Sets, working with Type is TBD *)
 
-Inductive llist (A : Set) : Set :=
-| nill  : llist A
-| conss : A -> llist A -> llist A.
+Inductive list' (A : Set) : Set :=
+| nil'  : list' A
+| cons' : A -> list' A -> list' A.
 
-Arguments nil {A}.
-Arguments conss {A} _ _.
+Arguments nil' {A}.
+Arguments cons' {A} _ _.
 
 Inductive tree (A B : Set) : Set :=
 | mempty : tree A B
 | mnode : A -> B -> tree A B -> tree A B -> tree A B.
 
+(* TODO: The Name X for parameters is currently unavailable *)
 
 (* ------- EDIT here ------ *)
 
@@ -26,6 +28,9 @@ Notation Fname := "treeF".
 (* ------------------------ *)
 
 
+(* To transform the datatype to it's functorial representation *)
+(* The first step is to add (X : Set) as it's last parameter *)
+(* This is what we do here: *)
 Definition newT : Type.
   let rec go x :=
     match x with
@@ -36,22 +41,17 @@ Definition newT : Type.
   go Ty.
 Defined.
 
-Definition newlist : tsf_ctors_ty (newT).
+(* Now we are going to edit the type of each constructor *)
+(* Following the same strategy as in `tutorial.v` *)
+Definition newT_ctors : tsf_ctors_ty (newT).
 Proof.
-  run_template_program (get_ctors T)
-                       (fun xs =>
-                          let xs := eval simpl in xs in
-                          let rec go xs :=
-                            lazymatch xs with
-                            | (?name, (existT_typed_term ?cty ?ctor)) :: ?xs =>
-                                let n := eval compute in (append name "F") in
-                                refine ((n, _) :: _); [
-                                  intros Hc R; unfold newT in R; tsf_interact ctor R
-                                | go xs ]
-                            | _ => exact []
-                            end
-                          in go xs).
 
+  (* We introduce the signature of the datatype as R, *)
+  (* and each one of it's constructor to the context  *)
+  tsf_ctors T (fun s => append s "F") tsf_interact; unfold newT in R.
+
+  (* Now we refine each one of R parameters with `refine (forall A, _)` *)
+  (* And apply each one to R, to build it's complete return type for each constructor *)
   all:
     let rec rector ty := match ty with
                              | forall (A : Set), ?U => refine (forall (A : Set), _);
@@ -61,22 +61,32 @@ Proof.
                              | _ => idtac
                          end in
     let Ty := type of R in
-    rector Ty;
+    rector Ty.
+
+  all:
+    (* This function gets the return type of a constructor *)
     let rec ctorTy ty := match ty with
                              | forall (_ : _), ?U => ctorTy U
                              | ?U => U
                          end in
     let Ty := type of ctor in
+    (* We remember the type of the constructor as Cty *)
     let Cty := ctorTy Ty in
+    (* Finally, to functorialize each constructor do the following: *)
     let rec go' ty := match ty with
-                          | Cty -> ?U => refine (X ->  _); go' U
+                        (* If Cty appears as a type, change it to X *)
+                          | forall (A : Cty), ?U => refine (forall (A : X), _); go' U
+                          (* Everything else remains unchanged *)
                           | forall A : ?S, ?U => refine (forall (A : S), _); go' U
+                          (* If Cty appears as the return type, change it to R *)
                           | Cty => exact R
                       end in
     go' Ty.
 Defined.
 
-Arguments newlist /.
+Arguments newT_ctors /.
+
+(* ------------ Metacoq Auxiliary Functions ------------ *)
 
 Fixpoint extract_params (t : term) (n : nat) : list (aname * term) :=
   match n with
@@ -113,6 +123,14 @@ Fixpoint count_tProds' (n : nat)  (t : term) : nat :=
 
 Definition count_tProds := count_tProds' 0.
 
+Notation "'unfolded' d" :=
+  ltac:(let y := eval unfold d in d in exact y) (at level 100, only parsing).
+
+(* ----------------------------------------------------- *)
+
+(* Update ind_gen from idt.v to accomodate the changes made to the parameters of *)
+(* the new datatype. *)
+(* ind_gen glues the datatype information together and then Metacoq synthesizes it *)
 
 #[universes(polymorphic)]
 Definition ind_gen' (name : ident) (ctors : list (ident * term))
@@ -129,7 +147,7 @@ Definition ind_gen' (name : ident) (ctors : list (ident * term))
       let indparam := map (fun '(s, t) => mk_indparam s t) params in
       let param_terms := map snd params in
       let lvls := flat_map get_level (ty_ret :: param_terms) in
-      let univ := Monomorphic_ctx (LevelSetProp.of_list [], ConstraintSet.empty) in
+      let univ := Monomorphic_ctx (LevelSetProp.of_list lvls, ConstraintSet.empty) in
       let ind' :=
         {| ind_finite := mind.(ind_finite);
            ind_npars := mind.(ind_npars) + 1;
@@ -149,8 +167,12 @@ Definition ind_gen' (name : ident) (ctors : list (ident * term))
   end.
 
 
+(* Synthesize the functorial representation of T *)
 MetaCoq Run (
+          (* Quote T into metacoq *)
           '(mind, i) <- tsf_get_mind ltac:(quote_term T (fun t => exact t));;
-          let ctorsT := ltac:(tsf_ctors_to_tm newlist) in
+          (* Quotes the new types of the constructors into metacoq *)
+          let ctorsT := ltac:(tsf_ctors_to_tm newT_ctors) in
+          (* Synthesize the functorial representation of T as Fname *)
           ind_gen' Fname ctorsT mind i
         ).
